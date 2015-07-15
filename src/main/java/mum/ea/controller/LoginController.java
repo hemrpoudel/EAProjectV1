28a4b130dd168427f68548e52c18c3ffbf1682fe
ajeshmahto <ajesh.mahto@gmai.com>
2015-07-14 19:25:38 -0500

    email sending

--- src/main/java/mum/ea/controller/LoginController.java
+++ src/main/java/mum/ea/controller/LoginController.java
@@ -1,96 +1,94 @@
 package mum.ea.controller;
 
+import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 
 import mum.ea.model.User;
 
 import org.apache.velocity.app.VelocityEngine;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.MailSender;
-import org.springframework.mail.SimpleMailMessage;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.velocity.VelocityEngineUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class LoginController {
-	
+
 	@Autowired
-	private MailSender mailSender;
+	private JavaMailSender mailSender;
+	@Autowired
 	private VelocityEngine velocityEngine;
 
-	
 	@RequestMapping("/login")
-	public String login( ) {
-		
-		
+	public String login() {
+
 		return "/login/login";
 	}
-	
-	
-	
-	 @RequestMapping("/loginfailed")
-	   public String loginFailed(){
-	    	
-	    	return "login/loginfailed";
-	    }
-	    
-	    @RequestMapping("/403")
-	    public String accessDenied(){
-	     	
-	     	return "login/accessdenied";
-	     }
-	    
-	    
-	    @RequestMapping("/logout")
-	    public String logout(){
-	    	
-	    	
-	     	
-	     	return "login/logout";
-	     }
-	
-	    @RequestMapping("/contact")
-		public String contact() {
-			
-			return "/contact";
-		}
-	    
-	    @RequestMapping("/404")
-		public String error404() {
-			
-	    	return "/404";
-		}
 
-	    @RequestMapping("/mail")
-	    public String sendMail(){
-	    	
-	    	User user = new User();
-	    	
-	    	SimpleMailMessage message = new SimpleMailMessage();
-	        message.setTo("bipinsharmaregmi@gmail.com");
-	        message.setSubject("test");
+	@RequestMapping("/loginfailed")
+	public String loginFailed() {
 
-//	        Map model = new HashMap();
-//            model.put("user", user);
-//	       String text = VelocityEngineUtils.mergeTemplateIntoString(
-//                    velocityEngine, "com/test/email-template.vm", model);
-//             message.setText(text, true);
-//	        message.setText("hello");
-//	        
-//	        
-//	        try
-//	        {
-//	        	mailSender.send(message);
-//	        }
-//	        
-//	        catch(Exception ex)
-//	        {
-//	        	System.out.println("can't send email"+ex);
-//	        }
-//	    	
-	        return "redirect:/home";
- 	    }
-	  
+		return "login/loginfailed";
+	}
+
+	@RequestMapping("/403")
+	public String accessDenied() {
+
+		return "login/accessdenied";
+	}
+
+	@RequestMapping("/logout")
+	public String logout() {
+
+		return "login/logout";
+	}
+
+	@RequestMapping("/contact")
+	public String contact() {
+
+		return "/contact";
+	}
+
+	@RequestMapping("/404")
+	public String error404() {
+
+		return "/404";
+	}
+
+	@RequestMapping("/mail")
+	public String sendMail() throws MessagingException {
+
+		final User user = new User();
+
+		MimeMessagePreparator preparator = new MimeMessagePreparator() {
+			public void prepare(MimeMessage mimeMessage) throws Exception {
+
+				MimeMessageHelper message = new MimeMessageHelper(mimeMessage);
+
+				message.setTo("ajesh.mahto@gmail.com");
+				message.setFrom("ajesh.mahto@gmail.com"); // could be
+															// parameterized...
+				Map<String, Object> model = new HashMap<String, Object>();
+				model.put("user", user);
+				@SuppressWarnings("deprecation")
+				String text = VelocityEngineUtils.mergeTemplateIntoString(
+						velocityEngine, "template/email-template.vm", model);
+				message.setText("<p><b>Hello Ajesh</b></p>" + text, true);
+
+			}
+		};
+		this.mailSender.send(preparator);
+
+		return "redirect:/index";
+	}
+
 }
